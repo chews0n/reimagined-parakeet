ada607d0d02152994f95855b6d0b9b98b784694d
RaoDeepika <dpkaraog@gmail.com>
2024-02-14 14:44:18 -0700

    added hello from deepika

--- main.py
+++ main.py
@@ -6,4 +6,6 @@ print('Hello World! The number is {}'.format(x))
 print(f'Hello World! The number is {x}') # different f string way.
 
 print('Hello World! The number is '+ str(x))
+
+print("Hello from Deepika!")
 # %%
