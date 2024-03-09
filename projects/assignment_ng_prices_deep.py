eda4331f114fba9c2130688c411ef2441114aee0
RaoDeepika <dpkaraog@gmail.com>
2024-03-08 17:36:49 -0700

    ..

--- projects/assignment_ng_prices_deep.py
+++ projects/assignment_ng_prices_deep.py
@@ -21,29 +21,35 @@ class APIDataFetcher:
     
     # Convert the simplified data to a DataFrame
         df = pd.DataFrame(simplified_data)
-    
+        return df
     # Save the DataFrame as a CSV file
-        df.to_csv(filename, index=False)
+        #df.to_csv(filename, index=False)
 
 
 # Usage
 url = "https://api.eia.gov/v2/natural-gas/pri/fut/data/"
-headers = {
-    "X-Params": json.dumps({
-        "frequency": "daily",
-        "data": ["value"],
-        "facets": {},
-        "start": "1990-01-01",
-        "end": "2000-01-01",
-        "sort": [{"column": "period", "direction": "desc"}],
-        "offset": 0,
-        "length": 5000
-    })
-}
-api_key = "80YB0XgTZLn682y6ggfsh4Q8Vy4GGyqt8YgdCFbR"         #  API key
-fetcher = APIDataFetcher(url, headers, api_key)
-data = fetcher.fetch_data()
-fetcher.save_as_csv(data, "ng_prices_output_90s.csv")
+
+data = pd.DataFrame()
+for year in range(2003, 2023):
+
+    headers = {
+        "X-Params": json.dumps({
+            "frequency": "daily",
+            "data": ["value"],
+            "facets": {},
+            "start": "1990-01-01",
+            "end": "2000-01-01",
+            "sort": [{"column": "period", "direction": "desc"}],
+            "offset": 0,
+            "length": 5000
+        })
+    }
+    api_key = "80YB0XgTZLn682y6ggfsh4Q8Vy4GGyqt8YgdCFbR"         #  API key
+    fetcher = APIDataFetcher(url, headers, api_key)
+    tmp = fetcher.fetch_data()
+    data.append(fetcher.save_as_csv(tmp, "ng_prices_90s.csv"), ignore_index = True)
+
+data.to_csv('ng_prices_90s.csv', index=False)
 print(data)
 
 
